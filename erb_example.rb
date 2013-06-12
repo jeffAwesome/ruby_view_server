72b8982333f42812bede4aa8c79a6f237202de9f
Jeff Richardson <jeff@masterpieceinteractive.com>
2013-06-12 02:46:44 -0500

    Updated erb_example with example data

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,16 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+content = "This is my obligatory content"
+template = ERB.new "My content says: <%= content %>"
+puts template.result(binding)
+
+
+myInfo = { "age" => 23, "occupation" => "Full Stack Web Developer" }
+template = ERB.new "I am <%= myInfo['age'] %> years old"
+puts template.result(binding)
+
+
+newTemplate = ERB.new "My occupation is <%= myInfo['occupation'] %> and I love it."
+puts newTemplate.result(binding)
